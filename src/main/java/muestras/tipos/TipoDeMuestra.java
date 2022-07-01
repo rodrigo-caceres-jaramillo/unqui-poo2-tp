96ddcfbb23a4c18d51a530542d3d64723ee5a5e4
Juan ignacio Garcia <juan.ignacio.garcia.02@gmail.com>
2022-07-01 16:16:21 -0300

    Se moidifico el contador de tipoDeMuestra

--- src/main/java/muestras/tipos/TipoDeMuestra.java
+++ src/main/java/muestras/tipos/TipoDeMuestra.java
@@ -1,6 +1,8 @@
 package main.java.muestras.tipos;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import main.java.muestras.Muestra;
 import main.java.muestras.Opinion;
@@ -10,6 +12,7 @@ public abstract class TipoDeMuestra {
 	public abstract void agregarOpinionA(Opinion opinion, Muestra muestra);
 
 	public int ocurrenciasDeTipoEn(ArrayList<Opinion> opiniones, TipoDeOpinion tipo) {
+		/*
 		int c = 0;
 		for (Opinion op : opiniones) {
 			if (op.getTipo() == tipo) {
@@ -17,5 +20,7 @@ public abstract class TipoDeMuestra {
 			}
 		}
 		return c;
+		*/
+		return  opiniones.stream().filter(op -> op.getTipo().equals(tipo)).collect(Collectors.toList()).size();
 	}
 }
